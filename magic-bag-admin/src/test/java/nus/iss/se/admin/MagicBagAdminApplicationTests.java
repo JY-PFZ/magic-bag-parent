c08437a572b7b4b4f1ca7ccc9599db914cc00500
JY-PFZ <72030441+JY-PFZ@users.noreply.github.com>
2025-10-24 11:51:40 +0800

    1. admin服务 2. cicd

---
+++ magic-bag-admin/src/test/java/nus/iss/se/admin/MagicBagAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.se.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicBagAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
